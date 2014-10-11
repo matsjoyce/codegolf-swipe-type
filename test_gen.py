458e43d012b87e57c6b8dbda245a945908e43920
Matthew Joyce <matsjoyce@gmail.com>
2014-10-11 10:13:46 +0100

    Enlarge word list

--- test_gen.py
+++ test_gen.py
@@ -2,8 +2,10 @@ import random
 
 words = open("wordlist").read().split("\n")
 with open("testlist", "w") as tests:
-    for i in range(50):
+    for i in range(100):
         word = random.choice(words)
         words.remove(word)
         swipe = input(word + ": ")
+        while swipe[0] != word[0] or swipe[-1] != word[-1]:
+            swipe = input(word + ": ")
         tests.write(word + " " + swipe + "\n")
