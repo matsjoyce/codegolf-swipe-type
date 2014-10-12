4f85e70f70417f7d57eeba4e3a4d85e5553c7953
Matthew Joyce <matsjoyce@gmail.com>
2014-10-12 20:33:03 +0100

    Add Martin Büttner's initial solution

---
+++ entries/regex-solver.rb
@@ -0,0 +1,6 @@
+words = File.readlines('wordlist').map(&:chomp)
+
+swipe = ARGV.shift
+puts words.select {|word| word[0] == swipe[0] &&
+                          word[-1] == swipe[-1] &&
+                          swipe[Regexp.new('^'+word.chars.join('.*')+'$')] }[0]
