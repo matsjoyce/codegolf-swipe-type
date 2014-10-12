2c09f18e01e4f5fe2e86a908d18eb3801dcbba58
Matthew Joyce <matsjoyce@gmail.com>
2014-10-12 21:17:21 +0100

    Update Regex Solver

--- entries/regex-solver.rb
+++ entries/regex-solver.rb
@@ -3,4 +3,5 @@ words = File.readlines('wordlist').map(&:chomp)
 swipe = ARGV.shift
 puts words.select {|word| word[0] == swipe[0] &&
                           word[-1] == swipe[-1] &&
-                          swipe[Regexp.new('^'+word.chars.join('.*')+'$')] }[0]
+                          swipe[Regexp.new('^'+word.chars.join('.*')+'$')] }
+          .sort_by {|word| word.size}[-1]
