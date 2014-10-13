c42b1629660d7d5beff220d4e2f142dac421c441
Matthew Joyce <matsjoyce@gmail.com>
2014-10-13 21:24:25 +0100

    Add Emil's solutions

---
+++ entries/turnaround.py
@@ -0,0 +1,34 @@
+import sys, csv, re
+
+wordlistfile = open('wordlist', 'r')
+wordlist = wordlistfile.read().split('\n')
+
+keyposfile = open('keypos.csv', 'r')
+keypos = dict((l, (float(x), float(y))) for l,x,y in csv.reader(keyposfile))
+
+#read input from command line argument
+input = sys.argv[1]
+
+#remove repeated letters
+input = ''.join(x for i,x in enumerate(input) if i==0 or x!=input[i-1])
+
+#find positions of letters on keyboard
+xpos = map(lambda l: keypos[l][0], input)
+ypos = map(lambda l: keypos[l][1], input)
+
+#check where the direction changes (neglect slight changes in x, e.g. 'edx')
+xpivot = [(t-p)*(n-t)<-1.5 for p,t,n in zip(xpos[:-2], xpos[1:-1], xpos[2:])]
+ypivot = [(t-p)*(n-t)<0 for p,t,n in zip(ypos[:-2], ypos[1:-1], ypos[2:])]
+pivot = [xp or yp for xp,yp in zip(xpivot, ypivot)]
+
+#the first and last letters are always pivots
+pivot = [True] + pivot + [True]
+
+#build regex
+regex = ''.join(x + ('+' if p else '*') for x,p in zip(input, pivot))
+regexobj = re.compile(regex + '$')
+
+#find all words that match the regex and output the longest one
+words = [w for w in wordlist if regexobj.match(w)]
+output = max(words, key=len) if words else input
+print output
